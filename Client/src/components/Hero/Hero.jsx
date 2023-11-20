a4e756be718296e23857f8b4939d78a2e36852b7
vanhuutoan27 <vhtoan27@gmail.com>
2023-11-20 19:43:40 +0700

    update amount product in cart

--- Client/src/components/Hero/Hero.jsx
+++ Client/src/components/Hero/Hero.jsx
@@ -9,39 +9,30 @@ function Hero() {
       <div className="hero-container flexCenter content innerWidth">
         {/* LEFT SIDE */}
         <div className="hero-left flexColStart">
-          <div className="hero-title">
-            <h1>
-              Summer best <br /> Collection <br />
-              2023
-            </h1>
-          </div>
-
-          <div className="hero-desc flexColStart">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              <br /> eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim <br />
-              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut <br /> aliquip ex
-              ea commodo consequat.
-            </p>
-          </div>
-
+          <h1 className="hero-title">Summer best Collection 2023</h1>
+          <p className="hero-desc flexColStart">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
           <Link to="#!" className="btn">
             Buy Now
           </Link>
         </div>
 
         {/* RIGHT SIDE */}
-        <div className="hero-right flexCenter">
-          <div className="hero-right-col" style={{ zIndex: '1' }}>
-            <h3>Best Selling item ever </h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-          </div>
-          <div className="hero-right-col" style={{ zIndex: '3' }}>
-            <h3>Get up to 60% off </h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-          </div>
-          <div className="image-container" style={{ zIndex: '2' }}>
+        <div className="hero-right">
+          <div className="image-container">
             <img src="../images/hero-image.png" alt="hero-image" />
+          </div>
+
+          <div className="hero-right-col col-1">
+            <h3>Best Selling item ever</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+          </div>
+          <div className="hero-right-col col-2">
+            <h3>Get up to 60% off</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
           </div>
         </div>
       </div>
--- Client/src/components/Navigation/Navigation.jsx
+++ Client/src/components/Navigation/Navigation.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import PersonIcon from '@mui/icons-material/Person';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShoppingBagRoundedIcon from '@mui/icons-material/ShoppingBagRounded';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 
@@ -12,10 +12,12 @@ function Navigation() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const location = useLocation();
 
+  // Toggles the state of the menu
   const handleMenuClick = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  // Adjusts menu state based on screen size
   useEffect(() => {
     const checkScreenSize = () => {
       if (window.innerWidth >= 992) {
@@ -24,82 +26,92 @@ function Navigation() {
     };
 
     window.addEventListener('resize', checkScreenSize);
-
     checkScreenSize();
 
     return () => window.removeEventListener('resize', checkScreenSize);
   }, []);
 
+  // Determines if a link is active based on the current path
   const isLinkActive = (pathname) => (location.pathname === pathname ? 'active' : '');
 
   return (
-    <div className="navigation-wrapper">
-      <div className="navigation-container content">
-        <div className="navigation-logo">
-          <Link to="#!">
-            <img src="../images/logo/Lit Sneaker Logo5.png" alt="Lit Sneaker" width={100} />
-          </Link>
-        </div>
-        <div className="navigation-menu">
-          <Link to="/" className={isLinkActive('/')}>
-            Home
-          </Link>
-          <Link to="/men" className={isLinkActive('/men')}>
-            Men
-          </Link>
-          <Link to="/women" className={isLinkActive('/women')}>
-            Women
-          </Link>
-          <Link to="/kids" className={isLinkActive('/kids')}>
-            Kids
-          </Link>
-          <Link to="/blog" className={isLinkActive('/blog')}>
-            Blog
-          </Link>
-          <Link to="/contact" className={isLinkActive('/contact')}>
-            Contact
-          </Link>
-        </div>
-        <div className="navigation-customer">
-          <div className="navigation-menu-mobile">
-            {isMenuOpen ? (
-              <CloseIcon className="navigation-icon" onClick={handleMenuClick} />
-            ) : (
-              <MenuIcon className="navigation-icon" onClick={handleMenuClick} />
-            )}
+    <div className={isMenuOpen ? 'dimmed-background' : ''}>
+      <div className="navigation-wrapper">
+        <div className="navigation-container content">
+          <div className="navigation-logo">
+            <Link to="#!">
+              <img src="../images/logo/Lit Sneaker Logo5.png" alt="Lit Sneaker" width={100} />
+            </Link>
           </div>
-          <Link to="#!">
-            <FavoriteIcon className="navigation-icon" />
-          </Link>
-          <Link to="#!">
-            <ShoppingBagRoundedIcon className="navigation-icon" />
-          </Link>
-          <Link to="#!">
-            <PersonIcon className="navigation-icon" />
-          </Link>
+          <div className="navigation-menu">
+            {/* Navigation links */}
+            <Link to="/" className={isLinkActive('/')}>
+              Home
+            </Link>
+            <Link to="/men" className={isLinkActive('/men')}>
+              Men
+            </Link>
+            <Link to="/women" className={isLinkActive('/women')}>
+              Women
+            </Link>
+            <Link to="/kids" className={isLinkActive('/kids')}>
+              Kids
+            </Link>
+            <Link to="/blog" className={isLinkActive('/blog')}>
+              Blog
+            </Link>
+            <Link to="/contact" className={isLinkActive('/contact')}>
+              Contact
+            </Link>
+          </div>
+          <div className="navigation-customer">
+            {/* Mobile menu and icons */}
+            <div className="navigation-menu-mobile">
+              {isMenuOpen ? (
+                <CloseIcon className="navigation-icon" onClick={handleMenuClick} />
+              ) : (
+                <MenuIcon className="navigation-icon" onClick={handleMenuClick} />
+              )}
+            </div>
+            <Link to="#!">
+              <FavoriteIcon className="navigation-icon" />
+            </Link>
+            <div className="cart-amount">
+              <Link to="#!">
+                <ShoppingCartIcon className="navigation-icon" />
+              </Link>
+              <div className="amount-number">
+                <Link to="#!">99+</Link>
+              </div>
+            </div>
+            <Link to="#!">
+              <PersonIcon className="navigation-icon" />
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className={`navigation-sub-menu ${isMenuOpen ? 'show' : ''}`}>
-        <ul>
-          <Link to="/">
-            <li className={isLinkActive('/')}>Home</li>
-          </Link>
-          <Link to="/men">
-            <li className={isLinkActive('/men')}>Men</li>
-          </Link>
-          <Link to="/women">
-            <li className={isLinkActive('/women')}>Women</li>
-          </Link>
-          <Link to="/kids">
-            <li className={isLinkActive('/kids')}>Kids</li>
-          </Link>
-          <Link to="/blog">
-            <li className={isLinkActive('/blog')}>Blog</li>
-          </Link>
-          <Link to="/contact">
-            <li className={isLinkActive('/contact')}>Contact</li>
-          </Link>
-        </ul>
+        <div className={`navigation-sub-menu ${isMenuOpen ? 'show' : ''}`}>
+          {/* Sub-menu for mobile */}
+          <ul>
+            <Link to="/">
+              <li className={isLinkActive('/')}>Home</li>
+            </Link>
+            <Link to="/men">
+              <li className={isLinkActive('/men')}>Men</li>
+            </Link>
+            <Link to="/women">
+              <li className={isLinkActive('/women')}>Women</li>
+            </Link>
+            <Link to="/kids">
+              <li className={isLinkActive('/kids')}>Kids</li>
+            </Link>
+            <Link to="/blog">
+              <li className={isLinkActive('/blog')}>Blog</li>
+            </Link>
+            <Link to="/contact">
+              <li className={isLinkActive('/contact')}>Contact</li>
+            </Link>
+          </ul>
+        </div>
       </div>
     </div>
   );
