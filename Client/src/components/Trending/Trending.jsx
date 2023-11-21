96788f406ed2b234ed74d9059061b855d28e9e41
vanhuutoan27 <vhtoan27@gmail.com>
2023-11-22 02:25:27 +0700

    update trending products

--- Client/src/components/Trending/Trending.jsx
+++ Client/src/components/Trending/Trending.jsx
@@ -1,13 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import './Trending.scss'
+import './Trending.scss';
 
 function Trending() {
+  // Example product data
+  const products = [
+    {
+      ProductName: "MEN'S RUNNING SHOE",
+      image: '../images/product-1.png',
+      Price: '$20',
+    },
+    {
+      ProductName: 'NIKE AIR MAX E77 SHOE',
+      image: '../images/product-2.png',
+      Price: '$25',
+    },
+    {
+      ProductName: 'SIMPLE FABRIC SHOE',
+      image: '../images/product-3.png',
+      Price: '$30',
+    },
+  ];
+
   return (
     <div className="trending-wrapper">
-      
+      <div className="trending-container content">
+        <div className="trending-header">
+          <h2 className="trending-title">Trending Products</h2>
+          <div className="trending-header-btn">
+            <Link className="btn">Explore More</Link>
+          </div>
+        </div>
+
+        <div className="trending-products">
+          {products.map((product) => (
+            <div className="trending-product">
+              <div className="trending-product-img">
+                <img src={product.image} alt={product.ProductName} />
+              </div>
+              <h3>{product.ProductName}</h3>
+              <div className="trending-product-price">
+                <span className="original-price">{product.Price}</span>
+              </div>
+              <div className="trending-product-btn">
+                <Link className="btn">Add To Cart</Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Trending
+export default Trending;
