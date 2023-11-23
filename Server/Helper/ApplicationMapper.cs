bee07ec68d05c98cdbc0f01874f2ce20b7e5504d
MetRyan <101856114+MetRyan@users.noreply.github.com>
2023-11-23 13:49:58 +0700

    1

--- Server/Helper/ApplicationMapper.cs
+++ Server/Helper/ApplicationMapper.cs
@@ -2,7 +2,14 @@
 
 namespace LitSneaker.Helper
 {
-    public class ApplicationMapper : Profile 
+    public class ApplicationMapper : Profile
     {
-    }
+        public ApplicationMapper () { 
+        
+       /*   CreateMap<UserVM, TbUser>().ReverseMap() //reverseMap 2 chieu
+                .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.TbAccount.Username))
+                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.TbAccount.Role));*/
+       
+        }
+}
 }
